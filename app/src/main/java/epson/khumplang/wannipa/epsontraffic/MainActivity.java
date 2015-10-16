15caf9defc0c3a4bfb35125577740080451106cf
wannipa <wannipa.k@eth.epson.co.th>
2015-10-16 09:38:58 +0700

    start Day2

---
+++ app/src/main/java/epson/khumplang/wannipa/epsontraffic/MainActivity.java
@@ -0,0 +1,33 @@
+package epson.khumplang.wannipa.epsontraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidge();
+
+
+    }   // Main Method
+
+    private void bindWidge() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+        
+    }
+
+}// Main Class นี่คือคลาสหลัก
