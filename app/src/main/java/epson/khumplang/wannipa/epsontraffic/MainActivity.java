3724c4138efa53113c9b4b7039052eba87642f48
wannipa <wannipa.k@eth.epson.co.th>
2015-10-16 15:08:25 +0700

    Create ListView

--- app/src/main/java/epson/khumplang/wannipa/epsontraffic/MainActivity.java
+++ app/src/main/java/epson/khumplang/wannipa/epsontraffic/MainActivity.java
@@ -27,8 +27,49 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //Create ListView
+        creatListView();
+
 
     }   // Main Method
+
+    private void creatListView() {
+
+        //Setup Source of Image
+        int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        String[]title1Strings = getResources().getStringArray(R.array.title1);
+        String[]title2Strings = getResources().getStringArray(R.array.title2);
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, iconInts, title1Strings, title1Strings);
+
+        trafficListView.setAdapter(objMyAdapter);
+
+
+
+
+
+    }
 
     private void buttonController() {
 
@@ -37,14 +78,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 //Sound effect
-                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.bird);
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.bird);
                 buttonMediaPlayer.start();
 
                 //Intent to WebView
                 Intent objIntent = new Intent(Intent.ACTION_VIEW);
-                objIntent.setData(Uri.parse("https://youtu.be/tkXOKkxc0BA"));startActivity(objIntent);
-            }
-        }); //tr
+                objIntent.setData(Uri.parse("https://youtu.be/tkXOKkxc0BA"));
+                startActivity(objIntent);
+
+
+            }    //event
+        });
 
     }
 
@@ -52,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
         trafficListView = (ListView) findViewById(R.id.listView);
         aboutMeButton = (Button) findViewById(R.id.button);
-        
+
     }
 
 }// Main Class นี่คือคลาสหลัก
